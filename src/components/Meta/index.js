6901b1e32c44927343a79393d2ffd8c192974b5a
Engga <17691328+xprimen@users.noreply.github.com>
2021-04-06 20:51:40 +0700

    instalasi Facebook Pixel

--- src/components/Meta/index.js
+++ src/components/Meta/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { FB_PIXEL_ID } from '../../lib/fbPixel'
 
 const Meta = (props) => {
     const router = useRouter()
@@ -25,6 +26,30 @@ const Meta = (props) => {
             <meta name="twitter:image" content="" /> */}
             <link rel="canonical" href={router.basePath} />
             {/* <script type="text/javascript" src="" ></script> */}
+            {/* Global Site Code Pixel - Facebook Pixel */}
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', ${FB_PIXEL_ID});
+              `,
+                }}
+            />
+            <noscript>
+                <img
+                    height="1"
+                    width="1"
+                    style={{ display: 'none' }}
+                    src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+                />
+            </noscript>
         </Head>
     )
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,9 +2,10 @@ import '../styles/globals.css'
 import '../styles/style.scss'
 import 'tailwindcss/tailwind.css'
 import Layout from '../components/Layout'
+import FacebookPixel from '../components/FacebookPixel'
 
 function MyApp({ Component, pageProps }) {
-  return <Layout><Component {...pageProps} /></Layout>
+  return <Layout><FacebookPixel><Component {...pageProps} /></FacebookPixel></Layout>
 }
 
 export default MyApp
